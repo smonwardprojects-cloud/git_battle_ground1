173041e45b7f2397d3c205faa6a50650cbbdff9a
sook mun <sookmun.wong@sagaos.com>
2025-11-14 16:01:41 +0800

    master testing

--- file1.php
+++ file1.php
@@ -3,46 +3,5 @@
 echo "can be anything , change without commit";
 echo "hello";
 echo "bye";
-echo "merge to feature branch without conflict";
-
-echo "1";
-echo "2";
-echo "3";
-echo "4";
-echo "5";
-echo "6";
-echo "7";
-echo "8";
-echo "9";
-echo "10";
-echo "i just change something and want to commit";
-echo "test for merge master";
-echo "merge to feature branch with conflict";
-echo "feature branch";
-echo "mini feature branch";
-echo "feature verification";
-echo "feature 1.0.0";
-echo "feature 1.0.1";
-echo "feature 1.0.2";
 echo "master1";
-echo "HF01";
-echo "HF02";
-echo "HF02-1";
-echo "HF03-1";
-echo "HF03-2";
-echo "HF05-1";
-echo "HF05-2";
-echo "testing111";
-echo "testing111a";
-echo "testing syncing";
-echo "test log";
-echo "test log2";
-echo "test squash1";
-echo "test squash2";
-echo "test squash3";
-echo "test squash4";
-echo "test squash5";
-echo "test squash6";
-echo "abcd new commit";
-
 ?>
