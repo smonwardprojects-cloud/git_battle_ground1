d53c4dcd57de978bf75360622294ad8335bac7d8
sook mun <sookmun.wong@sagaos.com>
2025-12-05 16:14:06 +0800

    bing

--- file2.php
+++ file2.php
@@ -12,5 +12,6 @@ echo "testing merge";
 echo "testing merge with no commit";
 echo "testing merge with no commit no create new commit and fast forward";
 echo "hohoho";
+echo "bing bing bing";
 
 ?>
